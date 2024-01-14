4bd97e93cfe2397c99d5cd56bb717e52cd2ef7c4
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-14 22:01:43 +0000

    "Fix bug with comas rendering at the top oftable

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function writeToDocument(url) {
             //el.innerHTML += "<p>" + item.name + "</p>"; 
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, ""); //  /,/ means find ,. g means find all, replace with empty string
     });
 }
 
